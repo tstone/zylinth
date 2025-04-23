a7a47251427ce53b6f7bbf8df0504cfe8aead63a
tstone <wastingtape@gmail.com>
2025-04-22 23:26:39 -0600

    not important

--- zylinth/src/map/decoration.rs
+++ zylinth/src/map/decoration.rs
@@ -12,23 +12,23 @@ pub fn decorate_empty(input: &mut TileGrid<TuesdayTile>, rng: &mut impl Rng) {
 lazy_static! {
     static ref EMPTIES: Vec<ReplacementRule<TuesdayTile>> = vec![
         ReplacementRule {
-            condition: |src, _| { is_wall(src.above()) },
+            condition: |src, _| { is_wall_ish(src.above()) },
             replacements: vec![Replacement::this(TuesdayTile::EmptyDecoration1),],
             chance: 0.1,
-            apply_count: Some(1),
+            apply_count: Some(2),
             ..Default::default()
         },
         ReplacementRule {
-            condition: |src, _| { is_wall(src.above()) },
+            condition: |src, _| { is_wall_ish(src.above()) },
             replacements: vec![Replacement::this(TuesdayTile::EmptyDecoration2),],
             chance: 0.1,
-            apply_count: Some(1),
+            apply_count: Some(2),
             ..Default::default()
         },
     ];
 }
 
-fn is_wall(ctx: TileContext<TuesdayTile>) -> bool {
+fn is_wall_ish(ctx: TileContext<TuesdayTile>) -> bool {
     ctx == WallLeft
         || ctx == WallRight
         || ctx == WallAllCorner
